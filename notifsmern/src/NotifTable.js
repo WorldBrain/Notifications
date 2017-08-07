a8262390f8d47c3ace9f249f8016134b88d6163d
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-07 14:05:53 +0200

    create Table component

---
+++ notifsmern/src/NotifTable.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './css/table-style.css';
+
+const tableHeaders = Object.keys(data[0]);
+
+const TableHead = () => (
+  <tr>
+    {tableHeaders.map(label => (
+      <th>{label}</th>
+    ))}
+  </tr>
+)
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tbody>
+        {data.map(row => (
+          <tr key={row.title}>
+            <td>{row.id}</td>
+            <td>{row.title}</td>
+            <td>{row.body}</td>
+            <td>{row.date}</td>
+            <td>{row.views}</td>
+          </tr>
+        ))}
+      </tbody>
+    )
+  }
+}
+
+class Table extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div>
+        <table>
+          <TableHead />
+          <TableRow />
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table
