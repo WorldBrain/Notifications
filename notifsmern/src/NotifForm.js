2d783df0f02dbe577cdcd970ead7ae4eee46c754
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-07 14:43:05 +0200

    created form component to submit new notifications

---
+++ notifsmern/src/NotifForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+// import './css/form-style.css';
+
+class NotifForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '', text:''};
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleTitleChange(e){
+    this.setState({ title: e.target.value});
+  }
+  handleBodyChange(e) {
+    this.setState({ body: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('${this.state.title} and "${this.state.body}"')
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Notification Title'
+          value={this.state.title}
+          onChange={this.handleTitleChange} />
+        <input
+          type='text'
+          placeholder='Notification message'
+          value={ this.state.body }
+          onChange={ this.handleBodyChange } />
+        <submit
+          type='submit'
+          value= 'Post' />
+      </form>
+    )
+  }
+}
+
+export default NotifForm;
