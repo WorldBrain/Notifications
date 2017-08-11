2f5c9eb7c61c0ec2c0144d970129b607616bb1f3
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-11 10:39:19 +0200

    edited fetch API

--- notifsmern/javascript/form.js
+++ notifsmern/javascript/form.js
@@ -1,37 +1,67 @@
-var notifForm = document.getElementById('notif-form');
+// var notifForm = document.getElementById('notif-form');
+//
+// notifForm.addEventListener('submit', function(event) {
+//
+//   var headers = new Headers(); //tell headers we expect JSON back
+//   headers.set('Accept', 'application/json');
+//
+//   var notifForm = new NotifForm();//loop through input items and appends values
+//   for (var i = 0; i < notifForm.length; ++i) {
+//     notifForm.append(notifForm[i].name, notifForm[i].value);
+//   }
+//
+//   //give fetch url and form data options
+//   var url = 'http://localhost:4001/api/notifications';
+//   var fetchOptions = {
+//     method: 'POST',
+//     headers: headers,
+//     body: formData,
+//     mode: 'cors' //not sure if this is needed?
+//   };
+//
+//   var responsePromise = fetch(url, fetchOptions);
+//
+//   responsePromise //convert response into JSON object
+//   .then(function(response){
+//     return response.json();
+//   })
+//   .then(function(jsonData){
+//     console.log(jsonData);
+//     document.getElementById('results').innerText =
+//     JSON.stringify(jsonData);
+//   });
+//
+//   event.preventDefault();
+// });
 
-notifForm.addEventListener('submit', function(event) {
-
-  var headers = new Headers(); //tell headers we expect JSON back
-  headers.set('Accept', 'application/json');
-
-  var notifForm = new NotifForm();//loop through input items and appends values
-  for (var i = 0; i < notifForm.length; ++i) {
-    notifForm.append(notifForm[i].name, notifForm[i].value);
-  }
-
-  //give fetch url and form data options
-  var url = 'http://localhost:4001/api/notifications';
-  var fetchOptions = {
-    method: 'POST',
-    headers,
-    body: formData
-  };
-
-  var responsePromise = fetch(url, fetchOptions);
-
-  responsePromise //convert response into JSON object
-  .then(function(response){
+function createNotif (opts) {
+  fetch('http://localhost:4001/api/notifications', {
+    method: 'post',
+    body: JSON.stringify(opts)
+  }).then(function(response) {
     return response.json();
-  })
-  .then(function(jsonData){
-    console.log(jsonData);
-    document.getElementById('results').innerText =
-    JSON.stringify(jsonData);
+  }).then(function(data) {
+    console.log('created Notif:', data.html_url)
   });
+}
 
-  event.preventDefault();
-});
+function submitNotif() {
+  var content = document.querySelector('textarea').value;
+  if (content) {
+    createNotif({
+      description: 'Fetch API POST',
+      public: true,
+    });
+  } else {
+    console.log('please enter message');
+  }
+}
+
+var submitBtn = document.querySelector('button');
+submitBtn.addEventListener('click', submitNotif);
+
+
+
 
 // notifForm.onsubmit = function (e) {
 //   event.preventDefault();
