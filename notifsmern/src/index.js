1e024be2643740b6e27030381b81216d49774aca
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-11 17:43:55 +0200

    removed index.css reference

--- notifsmern/src/index.js
+++ notifsmern/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import NotifForm from './NotifForm';
 import Table from './NotifTable';
 import List from './NotifList';
