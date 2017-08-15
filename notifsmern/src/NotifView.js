514467179bbcb7cd67c045df0ac49f8d344112f0
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-15 18:22:54 +0200

    changes to notifview parent component

--- notifsmern/src/NotifView.js
+++ notifsmern/src/NotifView.js
@@ -1,2 +1,27 @@
 import NotifForm from './NotifForm';
 import NotifList from './NotifList';
+
+export default class NotifView extends React.Component {
+  submit() {
+      this.setState(function(prevState, props){
+        return
+      })
+  }
+  render() {
+
+  }
+}
+
+export default class NotifView extends React.Component {
+  componentWillMount() {
+      NotifView.onSubmit( result => {
+        this.setState({
+
+        })
+      }
+  })
+}
+render() {
+    
+  }
+}
