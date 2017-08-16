f96064fabd13adcf38cb3bd90d97992a84a5cbdc
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-16 15:02:07 +0200

    display notif date and add styling for list and form

--- notifsmern/src/NotifList.js
+++ notifsmern/src/NotifList.js
@@ -4,8 +4,9 @@ import './css/list-style.css';
 const NotifList = ({ notifications }) => (
   <div>
     {notifications.map(({ id, date, title, body }) => (//if notifications exist, then map notifications
-      <div key={date}>
+      <div className="list-style" key={date}>
         <h1>{title}</h1>
+        <h3>{date}</h3>
         <h3 className="bodytext">{body}</h3>
       </div>
     ))}
