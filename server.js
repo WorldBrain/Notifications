da81d1f72ebe64c452fe0515e559ce551adcc065
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-08-25 14:36:07 +0200

    set mongodb uri and specified express for production, added error handling for app.listen

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const port = process.env.port || 4002;
 
 //set up express app
 const app = express();
@@ -14,15 +15,13 @@ const corsOptions = {
 };
 
 //setting mongodb URI
-
-var mongouri = 'mongodb://rgsoc:rgsoc123@ds153003.mlab.com:53003/wbnotifications';
-// var mongouri = process.env.MONGO_URI
-//   || 'mongodb://localhost/wbnotification'; // TODO remove me
+const mongouri = process.env.PROD_MONGODB
+  || 'mongodb://localhost/wbnotification'; // TODO remove me
 
 // TODO run `heroku config:set MONGO_URI=mongodb://something.`mlab`
 // after the db has been created
 
-app.use(express.static('./public'));
+app.use(express.static(process.env.NODE_ENV === 'production' ? './build' : './public'));
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions))
@@ -45,6 +44,7 @@ app.use(function(err,req,res,next){
 
 
 
-app.listen(process.env.port||4002,function(){
-  console.log('now listening for requests');
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log('now listening for requests on port: %s', port);
 });
